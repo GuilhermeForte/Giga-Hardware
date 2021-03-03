ae6657c81b70cfcb7b9db58ce091c2b277c2d2a8
Guilherme Forte <guilhermeforte@ymail.com>
2021-03-03 16:02:49 -0300

    correcao codigo

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
 <h1>Giga hardware Solutions</h1>
 <p>Soluçoes de informatica para sua empresa de infraestrutura até o Software.</p>
-<div style="width:100%;display:flex;height:80px">
+<div style="width:100%;display:flex;height: 80px">
 <div style="width:50%;backgroung:blue"></div>
 <div style="width:50%;backgroung:red"></div>
 </div>
