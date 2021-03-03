536da88028965faaa4eddc24b7ca6e5088104b82
Guilherme Forte <guilhermeforte@ymail.com>
2021-03-03 16:08:39 -0300

    correcao 2

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
 <!DOCTYPE html>
 <html>
-<head>
-<title>Giga Hardware Solutions</title>
-</head>
-<body>
-
-<h1>Giga hardware Solutions</h1>
-<p>Soluçoes de informatica para sua empresa de infraestrutura até o Software.</p>
-<div style="width:100%;display:flex;height: 80px">
-<div style="width:50%;backgroung:blue"></div>
-<div style="width:50%;backgroung:red"></div>
-</div>
-</body>
+   <head>
+      <title>Giga Hardware Solutions</title>
+   </head>
+   <body>
+      <h1>Giga hardware Solutions</h1>
+      <p>Soluçoes de informatica para sua empresa de infraestrutura até o Software.</p>
+      <div style="width:100%;display:flex;height: 80px">
+         <div style="width:50%;background:blue"></div>
+         <div style="width:50%;background:red"></div>
+      </div>
+   </body>
 </html>
