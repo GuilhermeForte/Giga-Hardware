662b4b55939026563b96ba9ac1a82b6e251e7c75
Guilherme Forte <guilhermeforte@ymail.com>
2021-03-10 10:04:29 -0300

    alteração

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
       <h1>Giga hardware Solutions</h1>
       <p>Soluçoes de informatica para sua empresa de infraestrutura até o Software.</p>
       <div style="width:100%;display:flex;height: 80px">
-         <div style="width:50%;background:blue"></div>
+         <div style="width:50%;background:violet"></div>
          <div style="width:50%;background:red"></div>
       </div>
    </body>
